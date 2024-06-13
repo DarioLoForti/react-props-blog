055f2b0d8510df249cf424d3be25fb24c0f91cda
Dario Lo Forti <lofortidario@gmail.com>
2024-06-13 14:39:40 +0200

    Update Main component to include distinct tags and improve styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,9 +3,13 @@ import Card from "../Cards/Card";
 import posts from "../../data/posts.js";
 
 const Main = () => {
+
+  const distinctTags = [...new Set(posts.flatMap(post => post.tags))];
+
   return (
     <main className={mainStyle.main}>
-       {
+      <div className={mainStyle.top}>
+      {
         posts.map((p) => {
           return <Card 
           key={p.id} 
@@ -17,6 +21,17 @@ const Main = () => {
           />
         })
        }
+      </div>
+       
+
+       <div className={mainStyle.button}>
+        <h2>Tags</h2>
+        <ul>
+          {distinctTags.map((tag, i) => (
+            <li key={`tag${i}`}>{tag}</li>
+          ))}
+        </ul>
+       </div>
         
     </main>
   );
